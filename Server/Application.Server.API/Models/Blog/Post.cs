77dacd34ebfe7b8934eb2f92ffd86463d440a9e4
mrgrayhat <saeed_ict_msh@yahoo.com>
2021-03-05 16:39:45 +0330

    add modified date property

--- Server/Application.Server.API/Models/Blog/Post.cs
+++ Server/Application.Server.API/Models/Blog/Post.cs
@@ -8,6 +8,7 @@ namespace Application.Server.API.Models.Blog
         public string Title { get; set; }
         public string Body { get; set; }
         public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime? Modified { get; set; }
         public string Author { get; set; }
         public string Tags { get; set; }
         public string Thumbnail { get; set; }
