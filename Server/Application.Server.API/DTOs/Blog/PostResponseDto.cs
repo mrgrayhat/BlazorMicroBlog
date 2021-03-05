667d7360f314277cbe2b559d5853bab160963001
mrgrayhat <saeed_ict_msh@yahoo.com>
2021-03-05 16:37:32 +0330

    move dto and wrappers from shared

---
+++ Server/Application.Server.API/DTOs/Blog/PostResponseDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Application.Server.DTOs.Blog
+{
+    /// <summary>
+    /// the post response dto, used for send result to the client
+    /// </summary>
+    public class PostResponseDto
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? Modified { get; set; }
+        public string Author { get; set; }
+        public string Tags { get; set; }
+        public string Description { get; set; }
+        public string Thumbnail { get; set; }
+
+    }
+}
