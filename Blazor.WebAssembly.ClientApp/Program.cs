0fb10f762e22c2a5c42850e00b321efc30eede3b
mrgrayhat <saeed_ict_msh@yahoo.com>
2021-03-05 15:55:09 +0330

    Add Blazor HttpClient and register blog client.

--- Blazor.WebAssembly.ClientApp/Program.cs
+++ Blazor.WebAssembly.ClientApp/Program.cs
@@ -1,7 +1,7 @@
 using System;
-using System.Net.Http;
 using System.Threading.Tasks;
 using Blazor.WebAssembly.ClientApp.Services;
+using MicroBlog.BlogClient;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -15,12 +15,11 @@ namespace Blazor.WebAssembly.ClientApp
             builder.RootComponents.Add<App>("#app");
             // register app taost notifications service.
             builder.Services.AddScoped<ToastService>();
-            // if you want to run blazor client standalone (only wasm), set uri to server(api), otherwise server will proxy it's self address (Hosted Mode)..
-            // default is http://127.0.0.1:5001 | https
-            builder.Services.AddScoped(sp => new HttpClient
+            // if you want to run blazor client standalone (only wasm), set base address to server(api) uri, otherwise server will proxy it's self address (Hosted Mode).
+            // default is https://127.0.0.1:5001 | http 5000
+            builder.Services.AddHttpClient<IBlogClient, BlogClient>((cfg) =>
             {
-                BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
-                //BaseAddress = new Uri("http://localhost:5001")
+                cfg.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
             });
 
             await builder.Build().RunAsync();
