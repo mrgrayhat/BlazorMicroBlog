951db93929c8ff6529b986485a717040bb5ae25d
mrgrayhat <saeed_ict_msh@yahoo.com>
2021-03-08 23:40:23 +0330

    add login page

---
+++ src/Client/Pages/account/Login.razor.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using MicroBlog.Blazor.Client.Services.Auth;
+using MicroBlog.BlogClient;
+using Microsoft.AspNetCore.Components;
+
+namespace MicroBlog.Blazor.Client.Pages.account
+{
+    public partial class Login
+    {
+        private UserLoginDto _userForAuthentication = new UserLoginDto();
+
+        [Inject]
+        public IAuthenticationService AuthenticationService { get; set; }
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+        public bool ShowAuthError { get; set; }
+        public string Error { get; set; }
+
+        public async Task ExecuteLogin()
+        {
+            ShowAuthError = false;
+
+            LoginResponseDto result = await AuthenticationService.Login(_userForAuthentication);
+            if (!result.IsAuthSuccessful)
+            {
+                Error = result.ErrorMessage;
+                ShowAuthError = true;
+            }
+            else
+            {
+                NavigationManager.NavigateTo("/");
+            }
+        }
+    }
+}
