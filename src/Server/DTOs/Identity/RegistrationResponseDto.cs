3ae8c7ad02e443d95019a7965c74e580bf0f2032
mrgrayhat <saeed_ict_msh@yahoo.com>
2021-03-08 23:13:32 +0330

    add register dto and register response dto

---
+++ src/Server/DTOs/Identity/RegistrationResponseDto.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace MicroBlog.Server.DTOs.Identity
+{
+    public class RegistrationResponseDto
+    {
+        public bool IsSuccessfulRegistration { get; set; }
+        public IEnumerable<string> Errors { get; set; }
+    }
+}
---
+++ src/Server/DTOs/Identity/UserRegisterationDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace MicroBlog.Server.DTOs.Identity
+{
+    public class UserRegistrationDto
+    {
+        [Required(ErrorMessage = "Username is required.")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+        [Display(Name = "I agree to the terms and conditions")]
+        [Compare("IsTrue", ErrorMessage = "Please agree to Terms and Conditions")]
+        public bool AcceptEULA { get; set; }
+        
+        [JsonIgnore]
+        public bool IsTrue => true;
+    }
+}
