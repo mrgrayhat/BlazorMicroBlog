62e18955d034dc770b31e6975151fe65f786a1cb
mrgrayhat <saeed_ict_msh@yahoo.com>
2021-03-08 23:22:53 +0330

    add & configure BlogApi http clients such as blog, accounts & upload clients

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
-using MicroBlog.BlogClient;
+using Blazored.LocalStorage;
 using MicroBlog.Blazor.Client.Services;
+using MicroBlog.Blazor.Client.Services.Auth;
+using MicroBlog.BlogClient;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -15,14 +18,22 @@ namespace MicroBlog.Blazor.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
             builder.Services.AddScoped<ToastService>();
+            builder.Services.AddAuthorizationCore();
+            builder.Services.AddBlazoredLocalStorage();
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+            builder.Services.AddScoped<AuthenticationStateProvider, AuthStateProvider>();
 
+            builder.Services.AddHttpClient<IAccountsClient, AccountsClient>((cfg) =>
+            {
+                cfg.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
+            });
             builder.Services.AddHttpClient<IBlogClient, BlogClient.BlogClient>((cfg) =>
             {
                 cfg.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
             });
-            builder.Services.AddHttpClient<IUploadClient, BlogClient.UploadClient>((cfg) =>
+            builder.Services.AddHttpClient<IUploadClient, UploadClient>((cfg) =>
             {
                 cfg.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
             });
