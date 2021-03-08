82bf3fbc76b23065394120197d7e328611dbfecd
mrgrayhat <saeed_ict_msh@yahoo.com>
2021-03-08 23:13:15 +0330

    add login dto and login response dto

---
+++ src/Server/DTOs/Identity/LoginResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MicroBlog.Server.DTOs.Identity
+{
+    public class LoginResponseDto
+    {
+        public bool IsAuthSuccessful { get; set; }
+        public string ErrorMessage { get; set; }
+        public string Token { get; set; }
+    }
+}
---
+++ src/Server/DTOs/Identity/UserLoginDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MicroBlog.Server.DTOs.Identity
+{
+    public class UserLoginDto
+    {
+        [Required(ErrorMessage = "Username/Email is required.")]
+        public string UsernameOrEmail { get; set; }
+        [Required(ErrorMessage = "Password is required.")]
+        public string Password { get; set; }
+        public bool RememberMe { get; set; } = false;
+    }
+}
