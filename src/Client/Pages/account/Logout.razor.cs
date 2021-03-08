bc89bf64a65d7d13dd798728c03c6aa24ecee734
mrgrayhat <saeed_ict_msh@yahoo.com>
2021-03-08 23:40:31 +0330

    add logout page

---
+++ src/Client/Pages/account/Logout.razor.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using MicroBlog.Blazor.Client.Services.Auth;
+using Microsoft.AspNetCore.Components;
+
+namespace MicroBlog.Blazor.Client.Pages.account
+{
+    public partial class Logout
+    {
+        [Inject]
+        public IAuthenticationService AuthenticationService { get; set; }
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await AuthenticationService.Logout();
+            NavigationManager.NavigateTo("/");
+        }
+    }
+}
