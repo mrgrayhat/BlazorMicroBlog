cdc737cdd0fb66be15df24fcbc696a5598e9e137
mrgrayhat <saeed_ict_msh@yahoo.com>
2021-03-08 23:02:23 +0330

    add Identity Auth & Jwt

--- src/Server/Startup.cs
+++ src/Server/Startup.cs
@@ -1,9 +1,6 @@
-using System.Linq;
 using MicroBlog.Server.API.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -40,7 +37,6 @@ namespace MicroBlog.Server
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseSerilogRequestLogging(); // req/res logging
@@ -67,6 +63,9 @@ namespace MicroBlog.Server
             app.UseStaticFiles();
 
             app.UseRouting();
+            
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
