f61f0998351cfb2dbdc5e07d135929d72cd78c37
mrgrayhat <saeed_ict_msh@yahoo.com>
2021-03-09 16:27:53 +0330

    add HttpClientInterceptor and ui loading progress

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -2,12 +2,13 @@ using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Blazored.LocalStorage;
-using MicroBlog.Blazor.Client.Services;
 using MicroBlog.Blazor.Client.Services.Auth;
+using MicroBlog.Blazor.Client.Services.ToastNotification;
 using MicroBlog.BlogClient;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Toolbelt.Blazor.Extensions.DependencyInjection;
 
 namespace MicroBlog.Blazor.Client
 {
@@ -20,25 +21,29 @@ namespace MicroBlog.Blazor.Client
             builder.Services.AddScoped<ToastService>();
             builder.Services.AddAuthorizationCore();
             builder.Services.AddBlazoredLocalStorage();
+            builder.Services.AddHttpClientInterceptor();
+            builder.Services.AddLoadingBar();
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
             builder.Services.AddScoped<AuthenticationStateProvider, AuthStateProvider>();
 
-            builder.Services.AddHttpClient<IAccountsClient, AccountsClient>((cfg) =>
+            builder.Services.AddHttpClient<IAccountsClient, AccountsClient>((sp, cl) =>
             {
-                cfg.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
+                cl.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
+                cl.EnableIntercept(sp);
             });
-            builder.Services.AddHttpClient<IBlogClient, BlogClient.BlogClient>((cfg) =>
+            builder.Services.AddHttpClient<IBlogClient, BlogClient.BlogClient>((sp, cl) =>
             {
-                cfg.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
+                cl.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
+                cl.EnableIntercept(sp);
             });
-            builder.Services.AddHttpClient<IUploadClient, UploadClient>((cfg) =>
+            builder.Services.AddHttpClient<IUploadClient, UploadClient>((sp, cl) =>
             {
-                cfg.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
+                cl.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
+                cl.EnableIntercept(sp);
             });
 
-            await builder.Build().RunAsync();
+            await builder.Build().UseLoadingBar().RunAsync();
         }
     }
 }
