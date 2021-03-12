3fe9019ac2cd2d96355ed96ccf558970b77c7374
mrgrayhat <saeed_ict_msh@yahoo.com>
2021-03-12 23:26:45 +0330

    add ctors

--- src/Server/DTOs/Identity/LoginResponseDto.cs
+++ src/Server/DTOs/Identity/LoginResponseDto.cs
@@ -2,6 +2,16 @@
 {
     public class LoginResponseDto
     {
+        public LoginResponseDto()
+        {
+
+        }
+        public LoginResponseDto(string error)
+        {
+            IsAuthSuccessful = false;
+            ErrorMessage = error;
+        }
+
         public bool IsAuthSuccessful { get; set; }
         public string ErrorMessage { get; set; }
         public string Token { get; set; }
