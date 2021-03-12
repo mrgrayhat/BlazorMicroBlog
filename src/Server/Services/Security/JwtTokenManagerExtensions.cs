779110fd4736f36f76993abfef20b34d2301196c
mrgrayhat <saeed_ict_msh@yahoo.com>
2021-03-12 23:37:34 +0330

    add new claims to user token such as email, cultuure, avatar url

--- src/Server/Services/Security/JwtTokenManagerExtensions.cs
+++ src/Server/Services/Security/JwtTokenManagerExtensions.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
+using System.IO;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
+using MicroBlog.Server.Models.Identity;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
@@ -14,18 +17,20 @@ namespace MicroBlog.Server.Services.Security
     {
         public static SigningCredentials GetSigningCredentials(this IConfiguration jwtSettings)
         {
-            byte[] key = Encoding.UTF8.GetBytes(jwtSettings.GetSection("securityKey").Value);
-            SymmetricSecurityKey secret = new SymmetricSecurityKey(key);
+            SymmetricSecurityKey secret = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.GetValue<string>("securityKey")));
 
             return new SigningCredentials(secret, SecurityAlgorithms.HmacSha256);
         }
-        public static async Task<List<Claim>> GetClaims(this IdentityUser user, UserManager<IdentityUser> userManager)
+        public static async Task<List<Claim>> GetClaims(this UserInfo user, UserManager<UserInfo> userManager, string webRootPath)
         {
             var claims = new List<Claim>
             {
-                new Claim(ClaimTypes.Name, user.Email)
+                new Claim(ClaimTypes.Name, user.UserName),
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.Country, user.Country),
+                new Claim(ClaimTypes.Locality,user.LocaleCulture),
+                new Claim("Avatar", user.Avatar)
             };
-
             var roles = await userManager.GetRolesAsync(user);
             foreach (var role in roles)
             {
