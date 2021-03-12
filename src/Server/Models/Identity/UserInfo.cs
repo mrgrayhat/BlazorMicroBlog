6545ee8c3d018a56e4b9541c09a80a858c3334ca
mrgrayhat <saeed_ict_msh@yahoo.com>
2021-03-12 23:33:59 +0330

    extend default identity user moedl, add new field such as avatar, culture ...

---
+++ src/Server/Models/Identity/UserInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using MicroBlog.Server.API.Models.Blog;
+using Microsoft.AspNetCore.Identity;
+
+namespace MicroBlog.Server.Models.Identity
+{
+    public class UserInfo : IdentityUser
+    {
+        public string Avatar { get; set; } = @"\site\avatars\no-avatar.png";
+        public string Bio { get; set; }
+        public DateTime RegisterDate { get; set; } = DateTime.Now;
+        public string Sex { get; set; } = "Unknown";
+        public byte Age { get; set; }
+        public string Country { get; set; }
+        public string LocaleCulture { get; set; } = "en-US";
+        public DateTime? LastActivityDate { get; set; }
+        public int Followers { get; set; }
+
+        public virtual ICollection<Post> Posts { get; set; }
+
+        public string RefreshToken { get; set; }
+        public DateTime RefreshTokenExpiryTime { get; set; }
+    }
+}
