d0d38e6136406cc5e43465bde7c8bc6c5f12e7d0
mrgrayhat <saeed_ict_msh@yahoo.com>
2021-03-12 23:30:11 +0330

    login/register response checking

--- src/Client/Services/Auth/AuthenticationService.cs
+++ src/Client/Services/Auth/AuthenticationService.cs
@@ -1,5 +1,6 @@
-﻿using System.Net.Http;
-using System.Net.Http.Headers;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Blazored.LocalStorage;
 using MicroBlog.BlogClient;
@@ -15,7 +16,6 @@ namespace MicroBlog.Blazor.Client.Services.Auth
     }
     public class AuthenticationService : IAuthenticationService
     {
-        //private readonly HttpClient _client;
         private readonly IAccountsClient _accountsClient;
         private readonly AuthenticationStateProvider _authStateProvider;
         private readonly ILocalStorageService _localStorage;
@@ -28,24 +28,31 @@ namespace MicroBlog.Blazor.Client.Services.Auth
 
         public async Task<LoginResponseDto> Login(UserLoginDto userLoginDto)
         {
-            var authResult = await _accountsClient.LoginAsync(userLoginDto);
-            if (!authResult.IsAuthSuccessful)
+            var authResult = new LoginResponseDto();
+            try
+            {
+                authResult = await _accountsClient.LoginAsync(userLoginDto);
+                if (!authResult.IsAuthSuccessful)
+                    return authResult;
+                await _localStorage.SetItemAsync("authToken", authResult.Token);
+                ((AuthStateProvider)_authStateProvider).NotifyUserAuthentication(authResult.Token);
+                return new LoginResponseDto { IsAuthSuccessful = true };
+            }
+            catch
+            {
                 return authResult;
-            await _localStorage.SetItemAsync("authToken", authResult.Token);
-            ((AuthStateProvider)_authStateProvider).NotifyUserAuthentication(userLoginDto.UsernameOrEmail);
-            //_client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", authResult.Token);
-            return new LoginResponseDto { IsAuthSuccessful = true };
+            }
+
         }
         public async Task Logout()
         {
             await _localStorage.RemoveItemAsync("authToken");
             ((AuthStateProvider)_authStateProvider).NotifyUserLogout();
-            //_client.DefaultRequestHeaders.Authorization = null;
         }
 
         public async Task<RegistrationResponseDto> RegisterUser(UserRegistrationDto userForRegistration)
         {
-            var registerResult = await _accountsClient.RegisterUserAsync(userForRegistration);
+            var registerResult = await _accountsClient.RegisterUserAsync(userForRegistration).ConfigureAwait(false);
             if (!registerResult.IsSuccessfulRegistration)
             {
                 return registerResult;
@@ -53,4 +60,5 @@ namespace MicroBlog.Blazor.Client.Services.Auth
             return new RegistrationResponseDto { IsSuccessfulRegistration = true };
         }
     }
+}
 }
