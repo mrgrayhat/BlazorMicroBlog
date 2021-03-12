8196a5414686403d6761a325883606237c394aa9
mrgrayhat <saeed_ict_msh@yahoo.com>
2021-03-12 23:34:28 +0330

    add author relation and FK

--- src/Server/Models/Blog/Post.cs
+++ src/Server/Models/Blog/Post.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using MicroBlog.Server.Models.Identity;
+using Microsoft.AspNetCore.Identity;
 
 namespace MicroBlog.Server.API.Models.Blog
 {
@@ -9,10 +11,12 @@ namespace MicroBlog.Server.API.Models.Blog
         public string Body { get; set; }
         public DateTime Created { get; set; } = DateTime.Now;
         public DateTime? Modified { get; set; }
-        public string Author { get; set; }
         public string Tags { get; set; }
         public string Thumbnail { get; set; }
         public string Description { get; set; }
+
+        public int AuthorId { get; set; }
+        public virtual UserInfo Author { get; set; }
 
     }
 }
