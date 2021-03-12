a31115e08c0307aae82aeb997258193e91637cb3
mrgrayhat <saeed_ict_msh@yahoo.com>
2021-03-13 00:06:29 +0330

    add ctors

--- src/Server/DTOs/Identity/RegistrationResponseDto.cs
+++ src/Server/DTOs/Identity/RegistrationResponseDto.cs
@@ -4,6 +4,16 @@ namespace MicroBlog.Server.DTOs.Identity
 {
     public class RegistrationResponseDto
     {
+        public RegistrationResponseDto()
+        {
+            IsSuccessfulRegistration = true;
+        }
+        public RegistrationResponseDto(IEnumerable<string> errors)
+        {
+            IsSuccessfulRegistration = false;
+            Errors = errors;
+        }
+
         public bool IsSuccessfulRegistration { get; set; }
         public IEnumerable<string> Errors { get; set; }
     }
