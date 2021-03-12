902318e468c1998cd812f8b188ab5d068be0db29
mrgrayhat <saeed_ict_msh@yahoo.com>
2021-03-12 23:15:21 +0330

    add global http interceptor exception

---
+++ src/Client/Services/Http/HttpResponseException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace MicroBlog.Blazor.Client.Services.Http
+{
+    [Serializable]
+    internal class HttpResponseException : Exception
+    {
+        public HttpResponseException()
+        {
+        }
+
+        public HttpResponseException(string message)
+            : base(message)
+        {
+        }
+
+        public HttpResponseException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        protected HttpResponseException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
