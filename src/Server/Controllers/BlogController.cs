7ff0393d8ffa4f35668c625f5458e9f6b12cb67e
mrgrayhat <saeed_ict_msh@yahoo.com>
2021-03-15 04:03:15 +0330

    add SwaggerResponses for better client side responses & status handling

--- src/Server/Controllers/BlogController.cs
+++ src/Server/Controllers/BlogController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using MicroBlog.Server.API.Infrastructure.Contexts;
 using MicroBlog.Server.API.Models.Blog;
@@ -12,6 +13,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using NSwag.Annotations;
 
 namespace MicroBlog.Server.Controllers
 {
@@ -42,23 +44,24 @@ namespace MicroBlog.Server.Controllers
         /// <returns></returns>
         [HttpGet]
         [AllowAnonymous]
-        [ProducesResponseType(200)]
+        [SwaggerResponse(HttpStatusCode.OK, typeof(PagedResponse<IEnumerable<PostResponseDto>>))]
         public async Task<ActionResult<PagedResponse<IEnumerable<PostResponseDto>>>> Index([FromQuery] int? pageSize, int page = 1)
         {
             pageSize = pageSize.GetValueOrDefault(DEFAULT_PAGE_SIZE);
             // get paged result from posts table and sort them by newer posts
             IEnumerable<Post> posts = await _blogDbContext.Posts
                 .AsNoTracking() // remove unnecessary tracking cost for larg data
-                .Include(u => u.Author)
+                .Include(a => a.Author)
                 .OrderByDescending(x => x.Created)
-                .ThenByDescending(x => x.Modified)
+                .ThenBy(x => x.Modified)
                 .Skip((page - 1) * pageSize.Value)
                 .Take(pageSize.Value)
                 .ToListAsync().ConfigureAwait(false);
             // get total posts number
-            int totalPosts = await _blogDbContext.Posts.CountAsync().ConfigureAwait(false);
+            int totalBlogPosts = await _blogDbContext.Posts.AsNoTracking()
+                .CountAsync().ConfigureAwait(false);
             // map ef query result to a DTO for client
-            var postsDto = posts.Select(p => new PostResponseDto
+            IEnumerable<PostResponseDto> postsDto = posts.Select(p => new PostResponseDto
             {
                 ID = p.ID,
                 Author = p.Author.UserName,
@@ -71,10 +74,10 @@ namespace MicroBlog.Server.Controllers
                 Thumbnail = p.Thumbnail
             });
 
-            _logger.LogInformation("{count} data sent to client", posts.Count());
+            _logger.LogInformation("{count} data sent to client", postsDto.Count());
             // return the paged response
             return Ok(new PagedResponse<IEnumerable<PostResponseDto>>
-                (postsDto, page, pageSize.Value, total: totalPosts));
+                (postsDto, page, pageSize.Value, total: totalBlogPosts));
         }
 
         // Get api/<controller>/1
@@ -85,9 +88,9 @@ namespace MicroBlog.Server.Controllers
         /// <returns></returns>
         [HttpGet("{id}")]
         [AllowAnonymous]
+        [SwaggerResponse(HttpStatusCode.OK, typeof(Response<PostResponseDto>))]
+        [SwaggerResponse(HttpStatusCode.NotFound, typeof(Response<PostResponseDto>))]
         //[ResponseCache(CacheProfileName = "30SecCache")]
-        [ProducesResponseType(200)]
-        [ProducesResponseType(404)]
         public async Task<ActionResult<Response<PostResponseDto>>> GetById(int id)
         {
             //Post post = await _blogDbContext.Posts.FindAsync(id);
@@ -120,18 +123,28 @@ namespace MicroBlog.Server.Controllers
         /// <param name="postDto">post data</param>
         /// <returns>created post id</returns>
         [HttpPost]
-        [ProducesResponseType(200)]
         [ProducesResponseType(401)]
-        [ProducesResponseType(404)]
+        [SwaggerResponse(HttpStatusCode.OK, typeof(Response<int>))]
+        [SwaggerResponse(HttpStatusCode.BadRequest, typeof(Response<int>))]
         public async Task<ActionResult<Response<int>>> Post(PostDto postDto)
         {
             if (!ModelState.IsValid)
-                return BadRequest(ModelState);
-            if (await _blogDbContext.Posts.FirstOrDefaultAsync(x => x.Title.Equals(postDto.Title)).ConfigureAwait(false) != null)
-            {
-                ModelState.AddModelError("Title", "A post with this title is exist, specify another unique title.");
-                return BadRequest(ModelState);
-            }
+                return BadRequest(new Response<int>
+                {
+                    Succeeded = false,
+                    Message = "Model state is not valid",
+                    Errors = ModelState.Values.SelectMany(e => e.Errors.Select(m => m.ErrorMessage))
+                });
+            //if (await _blogDbContext.Posts.FirstOrDefaultAsync(x => x.Title.Equals(postDto.Title)).ConfigureAwait(false) != null)
+            //{
+            //    ModelState.AddModelError("Title", "A post with this title is exist, specify another unique title.");
+            //    return BadRequest(new Response<int>
+            //    {
+            //        Succeeded = false,
+            //        Message = "Post title is not unique.",
+            //        Errors = ModelState.Values.SelectMany(e => e.Errors.Select(m => m.ErrorMessage))
+            //    });
+            //}
             Post mapped = new Post
             {
                 Author = await _userManager.FindByNameAsync(User.Identity.Name).ConfigureAwait(false),
@@ -152,17 +165,24 @@ namespace MicroBlog.Server.Controllers
         /// <summary>
         /// update a post
         /// </summary>
-        /// <param name="id">post id</param>
+        /// <param name="id">post id to edit</param>
         /// <param name="postDto">new post data</param>
         /// <returns></returns>
         [HttpPut("{id}")]
-        //[ProducesResponseType(200)]
-        //[ProducesResponseType(401)]
-        //[ProducesResponseType(404)]
+        [ProducesResponseType(401)]
+        [SwaggerResponse(HttpStatusCode.OK, typeof(Response<int>))]
+        [SwaggerResponse(HttpStatusCode.NotFound, typeof(Response<int>))]
+        [SwaggerResponse(HttpStatusCode.BadRequest, typeof(Response<int>))]
+
         public async Task<ActionResult<Response<int>>> Put(int id, PostDto postDto)
         {
             if (!ModelState.IsValid)
-                return BadRequest(ModelState);
+                return BadRequest(new Response<int>
+                {
+                    Succeeded = false,
+                    Message = "ModelState is not valid.",
+                    Errors = ModelState.Values.SelectMany(e => e.Errors.Select(m => m.ErrorMessage))
+                });
 
             //var post = await _blogDbContext.Posts.FindAsync(id).ConfigureAwait(false);
             var post = await _blogDbContext.Posts
@@ -171,7 +191,8 @@ namespace MicroBlog.Server.Controllers
             if (post is null)
                 return NotFound(new Response<int>($"Couldn't find any post with ID {id}."));
 
-            post.Author = await _userManager.FindByNameAsync(User.Identity.Name).ConfigureAwait(false);
+            post.Author = await _userManager.FindByNameAsync(User.Identity.Name)
+                .ConfigureAwait(false);
             post.Title = postDto.Title;
             post.Body = postDto.Body;
             post.Description = postDto.Description;
@@ -182,8 +203,8 @@ namespace MicroBlog.Server.Controllers
             _blogDbContext.Posts.Update(post);
             await _blogDbContext.SaveChangesAsync().ConfigureAwait(false);
 
-            return StatusCode(201, new Response<int>(post.ID));
-            //return Ok(new Response<int>(post.ID));
+            //return StatusCode(201, new Response<int>(post.ID));
+            return Ok(new Response<int>(post.ID));
         }
 
         // Delete api/<controller>/1
@@ -193,13 +214,13 @@ namespace MicroBlog.Server.Controllers
         /// <param name="id">post id</param>
         /// <returns></returns>
         [HttpDelete("{id}")]
-        [ProducesResponseType(200)]
         [ProducesResponseType(401)]
-        [ProducesResponseType(404)]
+        [SwaggerResponse(HttpStatusCode.OK, typeof(Response<int>))]
+        [SwaggerResponse(HttpStatusCode.NotFound, typeof(Response<int>))]
         public async Task<ActionResult<Response<int>>> DeleteAsync(int id)
         {
             // first time, do find query And Track the Record for future request's, so ef give it from the cache in next find's.
-            var post = await _blogDbContext.Posts.FindAsync(id);
+            var post = await _blogDbContext.Posts.FindAsync(id).ConfigureAwait(false);
             if (post is null)
                 return NotFound(new Response<int>($"Couln't find post with ID {id}."));
             // do delete and save
@@ -216,8 +237,9 @@ namespace MicroBlog.Server.Controllers
         /// <returns></returns>
         [HttpGet("search/{term}")]
         [AllowAnonymous]
-        [ProducesResponseType(200)]
-        [ProducesResponseType(404)]
+        [SwaggerResponse(HttpStatusCode.OK, typeof(Response<IEnumerable<PostResponseDto>>))]
+        [SwaggerResponse(HttpStatusCode.NotFound, typeof(Response<IEnumerable<PostResponseDto>>))]
+
         public async Task<ActionResult<Response<IEnumerable<PostResponseDto>>>> Search(string term)
         {
             var posts = await _blogDbContext.Posts
