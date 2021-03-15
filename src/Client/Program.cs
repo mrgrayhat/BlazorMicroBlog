b6fe330398ea4211a44b2d177581fbb9c8ce8aac
mrgrayhat <saeed_ict_msh@yahoo.com>
2021-03-15 04:00:50 +0330

    document codes, add logging

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Net.Http;
 using System.Threading.Tasks;
 using Blazored.LocalStorage;
 using MicroBlog.Blazor.Client.Services.Auth;
@@ -19,34 +18,49 @@ namespace MicroBlog.Blazor.Client
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
+            builder.Services.AddLogging();
+
+            // a service to show toast notifications/messages in ui
             builder.Services.AddScoped<ToastService>();
+            // custom Http Interceptor service, to handle http requests requirements.
             builder.Services.AddScoped<HttpInterceptorService>();
+            // enable auth features
             builder.Services.AddAuthorizationCore();
+            // service for working with client browser storage
             builder.Services.AddBlazoredLocalStorage();
+            // http interceptor service
             builder.Services.AddHttpClientInterceptor();
+            // loading progress service, used to track requests progress
             builder.Services.AddLoadingBar();
 
-
+            #region Auth Service and providers
             builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
             builder.Services.AddScoped<AuthenticationStateProvider, AuthStateProvider>();
+            #endregion
 
-            builder.Services.AddHttpClient<IAccountsClient, AccountsClient>((sp, cl) =>
+            #region MicroBlog Api Typed HttpClient Services & Configurations
+            builder.Services.AddHttpClient<IAccountsClient, AccountsClient>("AccountsClient", (sp, cl) =>
             {
                 cl.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
                 cl.EnableIntercept(sp);
-            }).AddHttpMessageHandler<HttpInterceptorService>();
-            builder.Services.AddHttpClient<IBlogClient, BlogClient.BlogClient>((sp, cl) =>
-            {
-                cl.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
-                cl.EnableIntercept(sp);
-            }).AddHttpMessageHandler<HttpInterceptorService>();
-            builder.Services.AddHttpClient<IUploadClient, UploadClient>((sp, cl) =>
-            {
-                cl.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
-                cl.EnableIntercept(sp);
-            }).AddHttpMessageHandler<HttpInterceptorService>();
+            }).AddHttpMessageHandler<HttpInterceptorService>(); // add delegate handler
 
-            await builder.Build().UseLoadingBar().RunAsync();
+            builder.Services.AddHttpClient<IBlogClient, BlogClient.BlogClient>("BlogClient", (sp, cl) =>
+             {
+                 cl.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
+                 cl.EnableIntercept(sp);
+             }).AddHttpMessageHandler<HttpInterceptorService>(); // add delegate handler
+
+            builder.Services.AddHttpClient<IUploadClient, UploadClient>("UploadClient", (sp, cl) =>
+             {
+                 cl.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
+                 cl.EnableIntercept(sp);
+             }).AddHttpMessageHandler<HttpInterceptorService>(); // add delegate handler
+            #endregion
+
+            await builder.Build()
+                .UseLoadingBar() // display a loading progress for http requests on ui
+                .RunAsync();
         }
     }
 }
