a4de417ba078ff47999cbb4bc5d6d791c49a819d
mrgrayhat <saeed_ict_msh@yahoo.com>
2021-03-15 03:56:08 +0330

    validation check and show error message

--- src/Client/Pages/account/Login.razor.cs
+++ src/Client/Pages/account/Login.razor.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using MicroBlog.Blazor.Client.Services.Auth;
 using MicroBlog.Blazor.Client.Services.Http;
+using MicroBlog.Blazor.Client.Services.ToastNotification;
 using MicroBlog.BlogClient;
 using Microsoft.AspNetCore.Components;
 
@@ -10,6 +11,8 @@ namespace MicroBlog.Blazor.Client.Pages.account
     public partial class Login
     {
         private UserLoginDto _userForAuthentication = new UserLoginDto();
+        [Inject]
+        public ToastService _toastService { get; set; }
         [Inject]
         public IAuthenticationService AuthenticationService { get; set; }
         [Inject]
@@ -25,10 +28,12 @@ namespace MicroBlog.Blazor.Client.Pages.account
             {
                 Error = result.ErrorMessage;
                 ShowAuthError = true;
+                _toastService.ShowToast("Something is wrong! Please double check again.", ToastLevel.ERROR);
                 StateHasChanged();
             }
             else
             {
+                _toastService.ShowToast("You have logged in successfully", ToastLevel.SUCCESS);
                 NavigationManager.NavigateTo("/", true);
             }
         }
