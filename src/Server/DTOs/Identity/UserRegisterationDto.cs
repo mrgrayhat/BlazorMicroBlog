ddf5bb6e4083731da4ded6c6a9277500d3343f11
mrgrayhat <saeed_ict_msh@yahoo.com>
2021-03-15 04:04:53 +0330

    improve validation attributes, add doc

--- src/Server/DTOs/Identity/UserRegisterationDto.cs
+++ src/Server/DTOs/Identity/UserRegisterationDto.cs
@@ -3,21 +3,33 @@ using System.Text.Json.Serialization;
 
 namespace MicroBlog.Server.DTOs.Identity
 {
+    /// <summary>
+    /// user register input model
+    /// </summary>
     public class UserRegistrationDto
     {
-        [Required(ErrorMessage = "Username is required.")]
+        [Required(ErrorMessage = "A unique username is required.")]
         public string Username { get; set; }
-        [Required(ErrorMessage = "Email is required.")]
-        [EmailAddress]
+
+        [Required(ErrorMessage = "A unique Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address.")]
         public string Email { get; set; }
-        [Required(ErrorMessage = "Password is required")]
+
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+
+        [Compare("Password", ErrorMessage = "The password and confirmation password doesn't match.")]
+        [DataType(DataType.Password)]
         public string ConfirmPassword { get; set; }
+
         [Display(Name = "I agree to the terms and conditions")]
         [Compare("IsTrue", ErrorMessage = "Please agree to Terms and Conditions")]
         public bool AcceptEULA { get; set; }
-        
+
+        /// <summary>
+        /// Just to check if Accept EULA is accepted or not, Not include in data
+        /// </summary>
         [JsonIgnore]
         public bool IsTrue => true;
     }
