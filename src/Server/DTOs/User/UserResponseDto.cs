27b289e3ad1667decad3b085658d55ee2977bff5
mrgrayhat <saeed_ict_msh@yahoo.com>
2021-03-15 04:05:21 +0330

    add user info result dto

---
+++ src/Server/DTOs/User/UserResponseDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MicroBlog.Server.DTOs.User
+{
+    public class UserResponseDto
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string Avatar { get; set; }
+        public string Bio { get; set; }
+        public string Sex { get; set; }
+        public string Country { get; set; }
+        public DateTime RegisterDate { get; set; }
+        public DateTime LastActivityDate { get; set; }
+        public int Followers { get; set; }
+        public int Posts { get; set; }
+        public string Email { get; set; }
+
+    }
+}
