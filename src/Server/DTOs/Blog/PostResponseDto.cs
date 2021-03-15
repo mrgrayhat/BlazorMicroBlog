8c5fda987fd48599f5738a8c98d4af858a1108b4
mrgrayhat <saeed_ict_msh@yahoo.com>
2021-03-15 04:04:08 +0330

    add doc

--- src/Server/DTOs/Blog/PostResponseDto.cs
+++ src/Server/DTOs/Blog/PostResponseDto.cs
@@ -1,20 +1,51 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace MicroBlog.Server.DTOs.Blog
 {
     /// <summary>
-    /// the post response dto, used for send result to the client
+    /// Blog Post Result Model, include post information/data
     /// </summary>
     public class PostResponseDto
     {
+        /// <summary>
+        /// the post id in db
+        /// </summary>
         public int ID { get; set; }
+        /// <summary>
+        /// post short/header text
+        /// </summary>
         public string Title { get; set; }
+        /// <summary>
+        /// post content data
+        /// </summary>
+        [DataType(DataType.MultilineText)]
         public string Body { get; set; }
+        /// <summary>
+        /// creation time
+        /// </summary>
+        [DataType(DataType.DateTime)]
         public DateTime Created { get; set; }
+        /// <summary>
+        /// latest edit/update
+        /// </summary>
+        [DataType(DataType.DateTime)]
         public DateTime? Modified { get; set; }
+        /// <summary>
+        /// post writer
+        /// </summary>
         public string Author { get; set; }
+        /// <summary>
+        /// post tags, each tag seperated by semicolon (;)
+        /// </summary>
         public string Tags { get; set; }
+        /// <summary>
+        /// post explanation or footer
+        /// </summary>
         public string Description { get; set; }
+        /// <summary>
+        /// post thumbnail image preview (small picture)
+        /// </summary>
         public string Thumbnail { get; set; }
 
     }
