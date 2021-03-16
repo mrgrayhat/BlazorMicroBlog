d2cbc9fdfbb945237f08272ec3338f4dd676c614
mrgrayhat <saeed_ict_msh@yahoo.com>
2021-03-16 15:47:55 +0330

    add refresh token prop

--- src/Server/DTOs/Identity/LoginResponseDto.cs
+++ src/Server/DTOs/Identity/LoginResponseDto.cs
@@ -16,5 +16,6 @@
         public bool IsAuthSuccessful { get; set; }
         public string ErrorMessage { get; set; }
         public string Token { get; set; }
+        public string RefreshToken { get; set; }
     }
 }
